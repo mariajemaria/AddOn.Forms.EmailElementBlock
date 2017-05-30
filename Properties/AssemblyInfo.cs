9d9d03e52fba58225949e35d3797fd7a2d60547b
marija.jemuovic <marija.jemuovic@mogul.com>
2017-05-30 15:02:05 +0200

    Just an initial add-on structure before actually building the add-on

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AddOn.Forms.EmailElementBlock")]
+[assembly: AssemblyDescription("E-mail element block for Episerver Forms")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("marija.jemuovic")]
+[assembly: AssemblyProduct("AddOn.Forms.EmailElementBlock")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e1fde95f-587b-449e-b003-0e1b419bc67a")]
+
+[assembly: AssemblyVersion("1.0.*")]
---
+++ README.md
@@ -0,0 +1,6 @@
+
+[![Platform](https://img.shields.io/badge/Episerver%20Forms-4.5-orange.svg?style=flat)](http://world.episerver.com/add-ons/episerver-forms/)
+
+# AddOn.Forms.EmailElementBlock
+
+E-mail element block for Episerver Forms
